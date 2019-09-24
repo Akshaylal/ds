f65a0ff19cb16cfce079d8dca28572205b613e1e
Akshaylal <akshaylals2000@gmail.com>
2019-09-24 15:08:19 +0530

    incomplete

---
+++ infix-postfix.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#define STACK_MAX 100
+
+int stack[STACK_MAX], top = -1;
+
+void push(char key){
+	if(STACK_MAX <= top){
+		printf("Stack Overflow\n");
+	}else{
+		s[++top] = key;	
+	}
+}
+
+char pop(){
+	if(top < 0){
+		printf("Stack Underflow\n");
+	}else{
+		return s[top--];
+	}
+}
+
+int ISP(){
+	int y;
+	char x = pop();
+	switch(x){
+		case '(':y = 0;
+		break;
+		case '+':
+		case '-':y = 2;
+		break;
+		case '*':
+		case '/':
+		case '%':y = 4;
+		break;
+		case '^':y = 5;
+		break;
+	}
+	push(x);
+	return y;
+}
+
+int ICP(char inp){
+	switch(inp){
+		case '(':return 100;
+		break;
+		case '+':
+		case '-':return 1;
+		break;
+		case '*':
+		case '/':
+		case '%':return 3;
+		break;
+		case '^':return 6;
+		break;
+	}
+}
+
+void main(){
+	char str[100], i = -1;
+	push('(');
+	while((c = getchar()) != ')'){
+		if(
+	}
+}
