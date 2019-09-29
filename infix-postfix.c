d9cbbf7c39a34f6969623812c1783b4338d2b458
Akshaylal <akshaylals2000@gmail.com>
2019-09-29 10:10:17 +0530

    postfix

--- infix-postfix.c
+++ infix-postfix.c
@@ -23,36 +23,23 @@ char pop(){
 int ISP(int inp){
 	switch(inp){
 		case '+':
-		case '-':
-			return 2;
-			break;
+		case '-':return 2;
 		case '*':
-		case '/':
-			return 4;
-			break;
+		case '/':return 4;
 		case '^':return 5;
-			break;
 		case '(':return 0;
-			break;
 	}
 }
 
 int ICP(char inp){
 	switch(inp){
 		case '+':
-		case '-':
-			return 1;
-			break;
+		case '-':return 1;
 		case '*':
-		case '/':
-			return 3;
-			break;
+		case '/':return 3;
 		case '^':return 6;
-			break;
 		case '(':return 9;
-			break;
 		case ')':return 0;
-			break;
 	}
 }
 
