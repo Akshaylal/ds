3fe3f3ee89f6a088d6d0e446fa2c8e925d430961
Akshaylal <akshaylals2000@gmail.com>
2019-11-12 22:20:03 +0530

    addition ok

--- polynomial.c
+++ polynomial.c
@@ -13,24 +13,6 @@ struct poly* getNode(){
 }
 
 
-struct poly* search(struct poly *header, int key){
-	if(header == NULL){
-		return NULL;
-	}else{
-		struct poly *ptr = header;
-		while((ptr->link != NULL) & (ptr->exp != key)){
-			ptr2 = ptr;
-			ptr = ptr->link;
-		}
-		if(ptr->exp != key){
-			return NULL;
-		}else{
-			return ptr2;
-		}
-	}
-}
-
-
 void read_poly(struct poly **header){
 	struct poly *pnew, *ptr;
 	int n, i;
@@ -52,64 +34,56 @@ void read_poly(struct poly **header){
 	}	
 }
 
-void display(struct poly **header){
-	struct poly *ptr = *header;
+struct poly* padd(struct poly *pptr, struct poly *qptr){
+	struct poly *h = getNode(), *rptr;
+	rptr = h;
+	while((pptr != NULL) & (qptr != NULL)){
+		rptr->link = getNode();
+		rptr = rptr->link;
+		if(pptr->exp == qptr->exp){
+			rptr->exp = pptr->exp;
+			rptr->coe = pptr->coe + qptr->coe;
+			pptr = pptr->link;
+			qptr = qptr->link;
+		}else if(pptr->exp > qptr->exp){
+			rptr->exp = pptr->exp;
+			rptr->coe = pptr->coe;
+			pptr = pptr->link;
+		}else{
+			rptr->exp = qptr->exp;
+			rptr->coe = qptr->coe;
+			qptr = qptr->link;
+		}
+	}
+	if(pptr != NULL){
+		while(pptr != NULL){
+			rptr->link = getNode();
+			rptr = rptr->link;
+			rptr->exp = pptr->exp;
+			rptr->coe = pptr->coe;
+			pptr = pptr->link;
+		}
+	}
+	if(qptr != NULL){
+		while(qptr != NULL){
+			rptr->link = getNode();
+			rptr = rptr->link;
+			rptr->exp = qptr->exp;
+			rptr->coe = qptr->coe;
+			qptr = qptr->link;
+		}
+	}
+	return h->link;
+}
+
+void display(struct poly *ptr){
 	while(ptr != NULL){
 		printf("%dx^%d+", ptr->coe, ptr->exp);
 		ptr = ptr->link; 
 	}
 }
 
-int keys[25], key = -1;
-void add_ex(int n){
-	keys[++key] = n;
-}
-int s_key(int n){
-	int i;
-	for(i = 0; i <= key; ++i){
-		if(keys[i] == n)
-			return 1;
-		else
-			return 0;
-	}
-}
-void cl_key(){
-	key = -1;
-}
 
-void padd(struct poly *pt1, struct poly *pt2, struct poly **pt3){
-	*pt3 = NULL;
-	struct poly *temp, *ptrt;
-	while(pt1 != NULL){
-		ptrt = getNode();
-		if(*pt3 == NULL){
-			*pt3 = ptrt;
-		}
-		temp = search(pt2, pt1->exp);
-		ptrt->exp = pt1->exp;
-		if(temp == NULL){
-			ptrt->coe = pt1->coe;
-			pt1 = pt1->link;
-		}else{
-			add_ex(pt1->exp);
-			ptrt->coe = pt1->coe + pt2->coe;
-			pt1 = pt1->link;
-			pt2 = pt2->link;
-		}
-	}
-	while(pt2 != NULL){
-		ptrt = getNode();
-		if(*pt3 == NULL){
-			*pt3 = ptrt;
-		}
-		if(s_key(pt2->exp) != 1){
-			ptrt->exp = pt1->exp;
-		}
-		ptrt->exp = pt2->exp;
-		ptrt->coe = pt2->coe;
-	}
-	cl_key();
-}
 
 void main(){
 	struct poly *p1, *p2, *p3;
@@ -117,7 +91,11 @@ void main(){
 	read_poly(&p1);
 	printf("Second polynomial:\n");
 	read_poly(&p2);
-	padd(p1, p2, &p3);
-	display(&p3);
+	display(p1);
+	printf("\n");
+	display(p2);
+	printf("\n");
+	p3 = padd(p1, p2);
+	display(p3);
 	printf("\n");
 }
